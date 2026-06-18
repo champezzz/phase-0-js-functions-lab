e52792fc1a006d75e0f827411ba77d75ca442cdc
champezzz <osteenmurimi@gmail.com>
2026-06-18 15:57:58 +0300

    Functions js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
+function isPalindrome(word) {
+    const reversedWord = word.split("").reverse().join("");
+    return word === reversedWord;
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
